81570677d13979141bd959cb74ee5acc4773ab68
AngieLi98 <liceth5262@gmail.com>
2026-01-13 16:57:22 -0500

    esturctura model y repository servicio

--- src/main/java/com/generation/sPaw_backend/model/Groomer.java
+++ src/main/java/com/generation/sPaw_backend/model/Groomer.java
@@ -8,9 +8,5 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "Groomer")
 public class Groomer {
-    @Id
-    Private Long id;
-    @Column(
-            name = "titulo", length = 300;
-    )
+
 }
--- src/main/java/com/generation/sPaw_backend/repository/IServicioRepository.java
+++ src/main/java/com/generation/sPaw_backend/repository/IServicioRepository.java
@@ -1,4 +1,9 @@
-package com.generation.sPaw.repository;
+package com.generation.sPaw_backend.repository;
 
-public interface IServicioRepository {
+import com.generation.sPaw_backend.model.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IServicioRepository extends JpaRepository<Servicio, Long> {
 }
