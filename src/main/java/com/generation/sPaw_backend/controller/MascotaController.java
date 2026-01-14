d6467d7d1c70541cef868955bc3407c44502ded4
Victoria Patarroyo <victoria.patarroyo@gmail.com>
2026-01-14 16:18:04 -0500

    Ajustes a mascota

---
+++ src/main/java/com/generation/sPaw_backend/controller/MascotaController.java
@@ -0,0 +1,11 @@
+package com.generation.sPaw_backend.controller;
+
+import com.generation.sPaw_backend.service.IMascotaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mascotas")
+public interface MascotaController {
+	private final
+}
