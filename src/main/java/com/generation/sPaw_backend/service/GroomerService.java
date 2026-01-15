ea176bbfac8851bc4567d68b84fd0f46854492aa
juanrodriguezs1503 <juan.rodriguezs1503@gmail.com>
2026-01-14 20:48:36 -0500

    creacion entidad groomer

--- src/main/java/com/generation/sPaw_backend/service/GroomerService.java
+++ src/main/java/com/generation/sPaw_backend/service/GroomerService.java
@@ -1,4 +1,39 @@
-//package com.generation.sPaw_backend.service;
-//
-//public class GroomerService {
-//}
+package com.generation.sPaw_backend.service;
+
+import com.generation.sPaw_backend.model.Groomer;
+import com.generation.sPaw_backend.repository.IGroomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GroomerService implements IGroomerService {
+
+    private final IGroomerRepository groomerRepository;
+
+    public GroomerService(IGroomerRepository groomerRepository) {
+        this.groomerRepository = groomerRepository;
+    }
+
+    @Override
+    public List<Groomer> listar() {
+        return groomerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Groomer> buscarPorId(Long id) {
+        return groomerRepository.findById(id);
+    }
+
+    @Override
+    public Groomer guardar(Groomer groomer) {
+        return groomerRepository.save(groomer);
+
+    }
+
+    public void eliminar(Long id) {
+        groomerRepository.deleteById(id);
+    }
+
+}
--- src/main/java/com/generation/sPaw_backend/service/IGroomerService.java
+++ src/main/java/com/generation/sPaw_backend/service/IGroomerService.java
@@ -1,4 +1,14 @@
-//package com.generation.sPaw_backend.service;
-//
-//public interface IGroomerService {
-//}
+package com.generation.sPaw_backend.service;
+
+import com.generation.sPaw_backend.model.Groomer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IGroomerService {
+    List<Groomer> listar();
+    Optional<Groomer> buscarPorId(Long id);
+    Groomer guardar(Groomer groomer);
+
+    void eliminar(Long id);
+}
